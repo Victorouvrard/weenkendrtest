e46a1fdb0112c77a520f140569e5887a2e00257e
Ouvrard Victor <ouvrardsound@gmail.com>
2019-09-12 15:51:42 +0200

    gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_11_112250) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "destinations", force: :cascade do |t|
     t.string "destination_name"
     t.string "transport_price"
